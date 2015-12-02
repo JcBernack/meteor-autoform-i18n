207f9c61695d9abc917515898fe459345e0f771a
JcBernack <jc.bernack@gmail.com>
2015-12-02 20:31:01 +0100

    Refactoring

--- shared/lib.js
+++ shared/lib.js
@@ -6,10 +6,6 @@ function humanize(property) {
     });
 }
 
-function getKeys(jsonPath, key) {
-  return TAPi18n.__([jsonPath, key].join("."), { returnObjectTrees: true }) || {};
-}
-
 var defaults = {
   placeholder: "Type something...",
   firstOption: "Select something..."
@@ -18,43 +14,55 @@ var defaults = {
 SimpleSchema.prototype.i18n = function (jsonPath, options) {
   // do nothing on the server side
   if (Meteor.isServer) return;
-  // extend defaults
-  options = _.extend(defaults, options);
   // iterate over schema keys
   var schema = this._schema;
   _.each(schema, function (value, key) {
-
+    // skip invalid values
     if (!value) return;
-    var keys = getKeys(jsonPath, key);
+    // get translations
+    var keys = TAPi18n.__([jsonPath, key].join("."), { returnObjectTrees: true }) || {};
+    // extend defaults
+    var keyOptions = _.extend({}, defaults, { label: humanize(key) }, options);
+
+    /**
+     * Returns a function which returns a translation for the given attribute.
+     * The result is determined in the following order:
+     * 1. Translation
+     * 2. Default value set via options
+     * 3. Untranslated language key
+     * @param {String} attribute
+     * @returns {Function}
+     */
+    function translate(attribute) {
+      var languageKey = [jsonPath, key, attribute].join(".");
+      return function () {
+        var translated = TAPi18n.__(languageKey);
+        if (translated === key && keyOptions[attribute]) {
+          return keyOptions[attribute];
+        }
+        return translated;
+      };
+    }
 
     // make sure the key has an autoform property
     var s = schema[key];
     if (!s.autoform) s.autoform = {};
-
-    if (!s.autoform.placeholder && keys.placeholder) {
-      s.autoform.placeholder = function () {
-        return getKeys(jsonPath, key).placeholder || options.placeholder;
-      };
-    }
-
-    if (!s.autoform.options && keys.options) {
-      s.autoform.options = function () {
-        return getKeys(jsonPath, key).options;
-      };
-    }
-
-    if (!s.autoform.firstOption && keys.options) {
-      s.autoform.firstOption = function () {
-        return getKeys(jsonPath, key).placeholder || options.firstOption;
-      };
-    }
-
+    // translate label
     if (!s.label && keys.label) {
-      s.label = function () {
-        return getKeys(jsonPath, key).label || humanize(key);
-      };
+      s.label = translate("label");
     }
-
+    // translate placeholder
+    if (!s.autoform.placeholder && keys.placeholder) {
+      s.autoform.placeholder = translate("placeholder");
+    }
+    // translate options
+    if (!s.autoform.options && keys.options) {
+      s.autoform.options = translate("options");
+    }
+    // translate firstOption
+    if (!s.autoform.firstOption && keys.options) {
+      s.autoform.firstOption = translate("firstOption");
+    }
   });
   return schema;
 };
